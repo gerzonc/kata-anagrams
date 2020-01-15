62668ceed4066e2c2864a12c41c28ebd32917847
gerzonc <gerzonzcanario@gmail.com>
2020-01-14 23:39:47 -0400

    first method created and unit test added

---
+++ kata-anagrams.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace kata_anagrams.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- kata-anagrams/Program.cs
+++ kata-anagrams/Program.cs
@@ -2,8 +2,25 @@
 
 namespace kata_anagrams
 {
-    class Program
+    /// <summary>
+    /// TODO: Given a file containing one word per line, print out all the combinations of words that are anagrams; 
+    /// each line in the output contains all the words from the input that are anagrams of each other.
+    /// </summary>
+
+    public class Program
     {
+        /// <summary>
+        /// Check if one word is anagram of another
+        /// </summary>
+        /// <param name="word">Word with which you will make the comparison</param>
+        /// <param name="anagram">The word you'll like to know if it's anagram to another</param>
+        public bool IsAnagram(string word, string anagram)
+        {
+            if(word.Length != anagram.Length) return false;
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
